771d26fdd15349f22b985209720bda81f6b15a7a
박현민 <dndb4902@naver.com>
2022-10-28 10:09:00 +0900

    #아이템1.정적팩터리메서드 단점1,2

---
+++ src/main/java/com/example/effectivejava/item01/AdvancedSettings.java
@@ -0,0 +1,5 @@
+package com.example.effectivejava.item01;
+
+public class AdvancedSettings {
+    Settings settings;
+}
--- src/main/java/com/example/effectivejava/item01/Settings.java
+++ src/main/java/com/example/effectivejava/item01/Settings.java
@@ -1,15 +1,19 @@
 package com.example.effectivejava.item01;
 
+/**
+ * 이 클래스의 인스턴스는 #getInstance() 를 통해 사용한다.
+ * @see #getInstance()
+ */
 public class Settings {
     private boolean useAutoSteering;
     private boolean useABS;
     private Difficulty difficulty;
 
-    private Settings() {
+    private Settings() {    // private 이므로 상속 X
     }
 
     private static final Settings SETTINGS = new Settings();
-    public static Settings newInstance() {
+    public static Settings getInstance() {
         return SETTINGS;
     }
 
