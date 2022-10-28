96702384da141dafd79cd99c8c6b11bcf0d10efa
박현민 <dndb4902@naver.com>
2022-10-28 11:28:09 +0900

    #아이템1.완벽공략 - 열거타입

--- src/main/java/com/example/effectivejava/item01/Order.java
+++ src/main/java/com/example/effectivejava/item01/Order.java
@@ -1,9 +1,12 @@
 package com.example.effectivejava.item01;
 
+import java.util.Arrays;
+
 public class Order {
     private boolean prime;
     private boolean urgent;
     private Product product;
+    private OrderStatus orderStatus;
 
 
     public static Order primeOrder(Product product){
@@ -17,5 +20,13 @@ public class Order {
         order.urgent = true;
         order.product = product;
         return order;
+    }
+
+    public static void main(String[] args) {
+        Order order = new Order();
+        if(order.orderStatus == OrderStatus.DELIVERED){ // equals를 쓴다면 nullpointException이 발생
+            System.out.println("delivered");
+        }
+        Arrays.stream(OrderStatus.values()).forEach(System.out::println);
     }
 }
