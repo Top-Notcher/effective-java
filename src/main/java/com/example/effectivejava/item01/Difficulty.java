584696c0566f533a6c07f4cad3b11dfa3bb48aa5
박현민 <dndb4902@naver.com>
2022-10-28 09:24:04 +0900

    #아이템1.정적팩터리메서드 장점2 : 인스턴스 컨트롤

---
+++ src/main/java/com/example/effectivejava/item01/Difficulty.java
@@ -0,0 +1,4 @@
+package com.example.effectivejava.item01;
+
+public class Difficulty {
+}
---
+++ src/main/java/com/example/effectivejava/item01/Settings.java
@@ -0,0 +1,16 @@
+package com.example.effectivejava.item01;
+
+public class Settings {
+    private boolean useAutoSteering;
+    private boolean useABS;
+    private Difficulty difficulty;
+
+    private Settings() {
+    }
+
+    private static final Settings SETTINGS = new Settings();
+    public static Settings newInstance() {
+        return SETTINGS;
+    }
+
+}
