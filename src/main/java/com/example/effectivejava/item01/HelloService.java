50afa498a5776dc5b0b1b7bf6852fa650ff73ede
박현민 <dndb4902@naver.com>
2022-10-31 11:04:11 +0900

    #아이템1.완벽공략 - 인터페이스와 정적메서드

--- src/main/java/com/example/effectivejava/item01/HelloService.java
+++ src/main/java/com/example/effectivejava/item01/HelloService.java
@@ -10,4 +10,16 @@ public interface HelloService {
             return new EnglishHelloService();
         }
     }
+
+    static String hi(){
+        prepareMessage();
+        return "hi";
+    }
+    default String bye() {  // 기본메서드 : 이 인터페이스를 구현한 클래스들의 인스턴스들이 모두 사용가능
+        return "bye";
+    }
+
+    static private void prepareMessage() {
+
+    }
 }
