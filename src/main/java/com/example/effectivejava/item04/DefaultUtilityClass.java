2893c6276b449605c25409d832ceed50fe302b57
박현민 <dndb4902@naver.com>
2022-11-01 11:22:53 +0900

    #아이템4.핵심정리

---
+++ src/main/java/com/example/effectivejava/item04/DefaultUtilityClass.java
@@ -0,0 +1,8 @@
+package com.example.effectivejava.item04;
+
+//public class DefaultUtilityClass extends UtilityClass{
+//
+//    public static void main(String[] args) {
+//        DefaultUtilityClass utilityClass = new DefaultUtilityClass();
+//    }
+//}
