d075e8ee7c32def6f09ce2915491426f462eea49
박현민 <dndb4902@naver.com>
2022-11-10 17:11:17 +0900

    #아이템5.완벽공략15 - 스프링IoC

---
+++ src/main/java/com/example/effectivejava/item05/springIoc/SpellChecker.java
@@ -0,0 +1,24 @@
+package com.example.effectivejava.item05.springIoc;
+
+import com.example.effectivejava.item05.Dictionary;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SpellChecker {
+
+    private Dictionary dictionary;
+
+    public SpellChecker(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public boolean isValid(String word) {
+        return dictionary.contains(word);
+    }
+
+    public List<String> suggestions(String type) {
+        return dictionary.closeWordsTo(type);
+    }
+}
