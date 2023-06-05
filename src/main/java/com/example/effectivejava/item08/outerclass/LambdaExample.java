e7828e407f82b10a098c68fde856a75b76406cd6
박현민 <dndb4902@naver.com>
2023-06-05 10:26:14 +0900

    #아이템8.완벽 공략

---
+++ src/main/java/com/example/effectivejava/item08/outerclass/LambdaExample.java
@@ -0,0 +1,21 @@
+package com.example.effectivejava.item08.outerclass;
+
+import java.lang.reflect.Field;
+
+public class LambdaExample {
+    
+    private int value = 10;
+    
+    private Runnable instanceLambda = () -> {   // 캡처링을 하냐 안하냐에 따라 참조여부가 달라진다.
+        System.out.println(value);
+    };
+
+    public static void main(String[] args) {
+        LambdaExample example = new LambdaExample();
+        Field[] declaredFields = example.instanceLambda.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            System.out.println("field.getType() = " + field.getType());
+            System.out.println("field.getName() = " + field.getName());
+        }
+    }
+}
