0ed6904a2cfe574652aaf9d596ce5a37214408fc
박현민 <dndb4902@naver.com>
2023-06-06 14:28:11 +0900

    #아이템8.완벽 공략23 - AutoClosable

--- src/main/java/com/example/effectivejava/item08/autoclosable/AutoClosableIsGood.java
+++ src/main/java/com/example/effectivejava/item08/autoclosable/AutoClosableIsGood.java
@@ -1,18 +1,37 @@
 package com.example.effectivejava.item08.autoclosable;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class AutoClosableIsGood implements AutoCloseable {
 
-    private BufferedInputStream inputStream;
+//    private BufferedInputStream inputStream;
+//
+//    @Override
+//    public void close() throws Exception {
+//        try {
+//            inputStream.close();
+//        }catch (IOException e) {
+//            throw new RuntimeException("failed to close " + inputStream);
+//        }
+//    }
+
+    private BufferedReader reader;
+
+    public AutoClosableIsGood(String path) {
+        try {
+            this.reader = new BufferedReader(new FileReader(path));
+        }catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(path);
+        }
+    }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         try {
-            inputStream.close();
-        }catch (IOException e) {
-            throw new RuntimeException("failed to close " + inputStream);
+            reader.close();
+        } catch (IOException e) {
+            // logging
+            throw new RuntimeException(e);
         }
     }
 }
