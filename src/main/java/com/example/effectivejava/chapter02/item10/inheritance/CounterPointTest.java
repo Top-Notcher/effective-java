79fc985178dcf9ab0677429a64a9a45c53b830af
박현민 <dndb4902@naver.com>
2023-06-17 17:01:29 +0900

    #수정

--- src/main/java/com/example/effectivejava/chapter02/item10/inheritance/CounterPointTest.java
+++ src/main/java/com/example/effectivejava/chapter02/item10/inheritance/CounterPointTest.java
@@ -6,7 +6,7 @@ import com.example.effectivejava.chapter02.item10.Point;
 import java.util.Set;
 
 // CounterPoint를 Point로 사용하는 테스트 프로그램
-public class ColorPointTest {
+public class CounterPointTest {
     // 단위 원 안의 모든 점을 포함하도록 unitCircle을 초기화한다. (58쪽)
     private static final Set<Point> unitCircle = Set.of(
             new Point(1, 0), new Point(0,1),
@@ -20,12 +20,14 @@ public class ColorPointTest {
     public static void main(String[] args) {
         Point p1 = new Point(1, 0);
 //        Point p2 = new ColorPoint(1, 0, Color.RED);
+        // 상속이 아닌 위임을 이용한 ColorPoint
         Point p2 = new com.example.effectivejava.chapter02.item10.composition.ColorPoint(1, 0, Color.RED).asPoint();
 
         // true를 출력한다.
         System.out.println(onUnitCircle(p1));
 
         // true를 출력해야 하지만, Point의 equals가 getClass를 사용해 작성되었다면 그렇지 않다.
+        // false
         System.out.println(onUnitCircle(p2));
     }
 }
