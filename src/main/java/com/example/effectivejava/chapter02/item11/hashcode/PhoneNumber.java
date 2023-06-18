c0e7cb3996a54fc52b49d558cd59ce076f16b2f2
박현민 <dndb4902@naver.com>
2023-06-18 15:39:37 +0900

    #아이템11. 완벽 공략28 - 스레드 안전

--- src/main/java/com/example/effectivejava/chapter02/item11/hashcode/PhoneNumber.java
+++ src/main/java/com/example/effectivejava/chapter02/item11/hashcode/PhoneNumber.java
@@ -2,10 +2,7 @@ package com.example.effectivejava.chapter02.item11.hashcode;
 
 import com.example.effectivejava.chapter02.item10.Point;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class PhoneNumber {
     private final short areaCode, prefix, lineNum;
@@ -52,25 +49,33 @@ public class PhoneNumber {
 //    }
 //
 //    // 코드 11-3 한 줄짜리 hashCode 메서드 - 성능이 살짝 아쉽다 (71쪽)
-    @Override
-    public int hashCode() {
-        return Objects.hash(lineNum, prefix, areaCode);
-    }
-//
-//    // 해시코드를 지연 초기화하는 hashCode 메서드 - 스레드 안정성까지 고려해야 한다. (71쪽)
-//    private int hashCode;   // 자동으로 0으로 초기화된다.
-//
 //    @Override
 //    public int hashCode() {
-//        int result = hashCode;
-//        if(result == 0) {
-//            result = Short.hashCode(areaCode);
-//            result = 31 * result + Short.hashCode(prefix)
-//            result = 31 * result + Short.hashCode(lineNum);
-//            hashCode = result;
-//        }
-//        return result;
+//        return Objects.hash(lineNum, prefix, areaCode);
 //    }
+//
+    // 해시코드를 지연 초기화하는 hashCode 메서드 - 스레드 안정성까지 고려해야 한다. (71쪽)
+    private volatile int hashCode;   // 자동으로 0으로 초기화된다.
+
+    private Hashtable <PhoneNumber, String> phoneNumberStringHashtable; // synchronized type, thread-safety
+
+    @Override
+    public int hashCode() {
+        if(this.hashCode != 0) {
+            return hashCode;
+        }
+
+        synchronized (this) {
+            int result = hashCode;
+            if(result == 0) {
+                result = Short.hashCode(areaCode);
+                result = 31 * result + Short.hashCode(prefix);
+                result = 31 * result + Short.hashCode(lineNum);
+                hashCode = result;
+            }
+            return result;
+        }
+    }
 
     public static void main(String[] args) {
         Map<PhoneNumber, String> m = new HashMap<>();
