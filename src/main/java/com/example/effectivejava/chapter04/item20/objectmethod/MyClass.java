d4ca10a95dd785ac076fe8c8169e7a8333eb4b17
박현민 <dndb4902@naver.com>
2023-09-04 11:20:42 +0900

    #아이템20.완벽 공략 - 디폴트 메서드와 Object 메서드

---
+++ src/main/java/com/example/effectivejava/chapter04/item20/objectmethod/MyClass.java
@@ -0,0 +1,5 @@
+package com.example.effectivejava.chapter04.item20.objectmethod;
+
+public class MyClass extends Object implements MyInterface {
+
+}
---
+++ src/main/java/com/example/effectivejava/chapter04/item20/objectmethod/MyInterface.java
@@ -0,0 +1,14 @@
+package com.example.effectivejava.chapter04.item20.objectmethod;
+
+public interface MyInterface {
+//    default String toString() {
+//        return "myString";
+//    }
+//    default int hashCode() {
+//        return 10;
+//    }
+//    default boolean equals(Object o) {
+//        return true;
+//    }
+
+}
