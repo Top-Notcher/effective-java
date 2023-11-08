9cf98eab237644580b9c7b8b0ce940cc9e0dc6a4
박현민 <dndb4902@naver.com>
2023-11-08 16:47:37 +0900

    #아이템32.완벽 공략 - ThreadLocalRandom

---
+++ src/main/java/com/example/effectivejava/chapter05/item32/Random/RandomExample.java
@@ -0,0 +1,24 @@
+package com.example.effectivejava.chapter05.item32.Random;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomExample {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(10));
+
+        ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+        System.out.println(threadLocalRandom.nextInt(10));
+    }
+
+    private int value;
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int readValue = value;
+        if (readValue == expectedValue) {
+            value = newValue;
+        }
+        return readValue;
+    }
+}
